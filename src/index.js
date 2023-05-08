7d9764a39b2d2b379d25143664d51483b3eab505
Deadpool1 <atwood169@yahoo.com>
2023-05-07 23:09:29 -0500

    add get method

--- src/index.js
+++ src/index.js
@@ -3,54 +3,60 @@ import cors from 'cors'
 
 const API_KEY = "37c5032acb2b4e67a9f215631230205"
 let DEFAULT_CITY = "Minnetonka"
-let API_URL = (`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${DEFAULT_CITY}`, { mode: cors, })
+let API_URL = (`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${DEFAULT_CITY}`, {
+  method: 'GET',
+  mode: 'cors'
+})
 
 async function fetchWeatherData(city) {
-    try {
-        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}`, { mode: cors })
-        const data = await response.json()
-        console.log(data)
-        return data;
-    } catch (error) {
-        console.error(error, "error")
-    }
+  try {
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}`, {
+      method: 'GET',
+      mode: 'cors'
+    })
+    const data = await response.json()
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.error(error, "error")
+  }
 }
 
 // Converts Current date & time
 const getCurrentDateTime = () => {
-    const date = new Date()
-    const formattedDataTime = moment(date).format("dddd, Do MMM 'YY h:mm a")
-    return formattedDataTime
+  const date = new Date()
+  const formattedDataTime = moment(date).format("dddd, Do MMM 'YY h:mm a")
+  return formattedDataTime
 }
 
 // Display current city, date/time, temp and includes search bar
 const displayMainWeather = (city) => {
-    fetchWeatherData(city).then(data => {
-        const content = document.getElementById('content')
-        const condition = document.createElement('h1')
-        const cityName = document.createElement('h2')
-        const currentDate = document.createElement('p')
-        const temperature = document.createElement('h1')
-        const weatherImg = document.createElement('img')
-        weatherImg.src = `${data.current.condition.icon}`
-        condition.textContent = `${data.current.condition.text}`
-        cityName.textContent = city
-        currentDate.textContent = getCurrentDateTime()
-        temperature.textContent = `${Math.round(data.current.temp_f)} ºF`
-        temperature.classList.add('temp')
-        condition.classList.add('condition')
-        cityName.classList.add('city')
-        currentDate.classList.add('date')
+  fetchWeatherData(city).then(data => {
+    const content = document.getElementById('content')
+    const condition = document.createElement('h1')
+    const cityName = document.createElement('h2')
+    const currentDate = document.createElement('p')
+    const temperature = document.createElement('h1')
+    const weatherImg = document.createElement('img')
+    weatherImg.src = `${data.current.condition.icon}`
+    condition.textContent = `${data.current.condition.text}`
+    cityName.textContent = city
+    currentDate.textContent = getCurrentDateTime()
+    temperature.textContent = `${Math.round(data.current.temp_f)} ºF`
+    temperature.classList.add('temp')
+    condition.classList.add('condition')
+    cityName.classList.add('city')
+    currentDate.classList.add('date')
 
-        content.textContent = ''
-        content.appendChild(condition)
-        content.appendChild(cityName)
-        content.appendChild(currentDate)
-        content.appendChild(temperature)
-        content.appendChild(weatherImg)
-        content.appendChild(locationForm)
-    })
-}  
+    content.textContent = ''
+    content.appendChild(condition)
+    content.appendChild(cityName)
+    content.appendChild(currentDate)
+    content.appendChild(temperature)
+    content.appendChild(weatherImg)
+    content.appendChild(locationForm)
+  })
+}
 
 // Search For New Location 
 function handleLocationFormSubmit(event) {
@@ -87,97 +93,100 @@ displayMainWeather(DEFAULT_CITY);
 
 // Fetch extra current info
 const displayMoreInfo = () => {
-    fetchWeatherData(API_URL).then(data => {
-        console.log(data.current.feelslike_f)
+  fetchWeatherData(API_URL).then(data => {
+    console.log(data.current.feelslike_f)
 
-        const content2 = document.getElementById('content2')
-        
-        // Feels like section
-        const feelsLikeSection = document.createElement('div')
-        feelsLikeSection.classList.add('feels-like-section')
-        const feelsLikeIcon = document.createElement('img')
-        feelsLikeIcon.src = '../src/img/feelslike.png'
-        feelsLikeIcon.alt = 'Feels Like Icon'
-        const feelsInfo = document.createElement('div')
-        const feelsLikeText = document.createElement('h3')
-        feelsLikeText.textContent = 'Feels Like'
-        feelsLikeText.classList.add('feelslike-text')
-        const feelsLikeTemp = document.createElement('h2')
-        feelsLikeTemp.textContent = `${Math.round(data.current.feelslike_f)} ºF`
-        feelsLikeTemp.classList.add('feels-temp')
+    const content2 = document.getElementById('content2')
 
-        feelsLikeSection.appendChild(feelsLikeIcon)
-        feelsInfo.appendChild(feelsLikeText)
-        feelsInfo.appendChild(feelsLikeTemp)
-        feelsLikeSection.appendChild(feelsInfo)
+    // Feels like section
+    const feelsLikeSection = document.createElement('div')
+    feelsLikeSection.classList.add('feels-like-section')
+    const feelsLikeIcon = document.createElement('img')
+    feelsLikeIcon.src = '../src/img/feelslike.png'
+    feelsLikeIcon.alt = 'Feels Like Icon'
+    const feelsInfo = document.createElement('div')
+    const feelsLikeText = document.createElement('h3')
+    feelsLikeText.textContent = 'Feels Like'
+    feelsLikeText.classList.add('feelslike-text')
+    const feelsLikeTemp = document.createElement('h2')
+    feelsLikeTemp.textContent = `${Math.round(data.current.feelslike_f)} ºF`
+    feelsLikeTemp.classList.add('feels-temp')
 
-        // Humidity section
-        const humiditySection = document.createElement('div')
-        humiditySection.classList.add('humidity-section')
-        const humidityIcon = document.createElement('img')
-        humidityIcon.src = '../src/img/humidity-icon.png'
-        humidityIcon.alt = 'Humidity Icon'
-        const humidityInfo = document.createElement('div')
-        const humidityText = document.createElement('h3')
-        humidityText.textContent = 'Humidity'
-        humidityText.classList.add('humidity-text')
-        const humidityPercent = document.createElement('h2')
-        humidityPercent.textContent = `${data.current.humidity} %`
-        humidityPercent.classList.add('humidity-per')
+    feelsLikeSection.appendChild(feelsLikeIcon)
+    feelsInfo.appendChild(feelsLikeText)
+    feelsInfo.appendChild(feelsLikeTemp)
+    feelsLikeSection.appendChild(feelsInfo)
 
-        humidityInfo.appendChild(humidityText)
-        humidityInfo.appendChild(humidityPercent)
-        humiditySection.appendChild(humidityIcon)
-        humiditySection.appendChild(humidityInfo)
+    // Humidity section
+    const humiditySection = document.createElement('div')
+    humiditySection.classList.add('humidity-section')
+    const humidityIcon = document.createElement('img')
+    humidityIcon.src = '../src/img/humidity-icon.png'
+    humidityIcon.alt = 'Humidity Icon'
+    const humidityInfo = document.createElement('div')
+    const humidityText = document.createElement('h3')
+    humidityText.textContent = 'Humidity'
+    humidityText.classList.add('humidity-text')
+    const humidityPercent = document.createElement('h2')
+    humidityPercent.textContent = `${data.current.humidity} %`
+    humidityPercent.classList.add('humidity-per')
 
-        // Chance of Rain section
-        const chanceRainSection = document.createElement('div')
-        chanceRainSection.classList.add('chance-section')
-        const chanceRainIcon = document.createElement('img')
-        chanceRainIcon.src = '../src/img/chance-rain.png'
-        chanceRainIcon.alt = 'Chance Of Rain Icon'
-        const chanceInfo = document.createElement('div')
-        const chanceRainText = document.createElement('h3')
-        chanceRainText.textContent = 'Chance of Rain'
-        chanceRainText.classList.add('rain-text')
-        const chanceRainPer = document.createElement('h2')
-        chanceRainPer.textContent = `${data.forecast.forecastday[0].day.daily_chance_of_rain} %`
-        chanceRainPer.classList.add('rain-per')
+    humidityInfo.appendChild(humidityText)
+    humidityInfo.appendChild(humidityPercent)
+    humiditySection.appendChild(humidityIcon)
+    humiditySection.appendChild(humidityInfo)
 
-        chanceInfo.appendChild(chanceRainText)
-        chanceInfo.appendChild(chanceRainPer)
-        chanceRainSection.appendChild(chanceRainIcon)
-        chanceRainSection.appendChild(chanceInfo)
+    // Chance of Rain section
+    const chanceRainSection = document.createElement('div')
+    chanceRainSection.classList.add('chance-section')
+    const chanceRainIcon = document.createElement('img')
+    chanceRainIcon.src = '../src/img/chance-rain.png'
+    chanceRainIcon.alt = 'Chance Of Rain Icon'
+    const chanceInfo = document.createElement('div')
+    const chanceRainText = document.createElement('h3')
+    chanceRainText.textContent = 'Chance of Rain'
+    chanceRainText.classList.add('rain-text')
+    const chanceRainPer = document.createElement('h2')
+    chanceRainPer.textContent = `${data.forecast.forecastday[0].day.daily_chance_of_rain} %`
+    chanceRainPer.classList.add('rain-per')
 
-        // Wind Speed Section
-        const windSection = document.createElement('div')
-        windSection.classList.add('wind-section')
-        const windIcon = document.createElement('img')
-        windIcon.src = '../src/img/wind-icon.png'
-        windIcon.alt = 'Wind Speed Icon'
-        const windInfo = document.createElement('div')
-        const windText = document.createElement('h3')
-        windText.textContent = 'Wind Speed'
-        const windMph = document.createElement('h2')
-        windMph.textContent = `${data.current.wind_mph} mph`
+    chanceInfo.appendChild(chanceRainText)
+    chanceInfo.appendChild(chanceRainPer)
+    chanceRainSection.appendChild(chanceRainIcon)
+    chanceRainSection.appendChild(chanceInfo)
 
-        windInfo.appendChild(windText)
-        windInfo.appendChild(windMph)
-        windSection.appendChild(windIcon)
-        windSection.appendChild(windInfo)
+    // Wind Speed Section
+    const windSection = document.createElement('div')
+    windSection.classList.add('wind-section')
+    const windIcon = document.createElement('img')
+    windIcon.src = '../src/img/wind-icon.png'
+    windIcon.alt = 'Wind Speed Icon'
+    const windInfo = document.createElement('div')
+    const windText = document.createElement('h3')
+    windText.textContent = 'Wind Speed'
+    const windMph = document.createElement('h2')
+    windMph.textContent = `${data.current.wind_mph} mph`
 
-        content2.appendChild(feelsLikeSection)
-        content2.appendChild(humiditySection)
-        content2.appendChild(chanceRainSection)
-        content2.appendChild(windSection)
-    })
+    windInfo.appendChild(windText)
+    windInfo.appendChild(windMph)
+    windSection.appendChild(windIcon)
+    windSection.appendChild(windInfo)
+
+    content2.appendChild(feelsLikeSection)
+    content2.appendChild(humiditySection)
+    content2.appendChild(chanceRainSection)
+    content2.appendChild(windSection)
+  })
 }
 displayMoreInfo()
 
 
 // Set the API endpoint URL and parameters
 const city = DEFAULT_CITY;
-const url = (`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7`, { mode: cors });
+const url = (`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7`, {
+  method: 'GET',
+  mode: 'cors'
+});
 
 // Fetch the weather data from the API and display the forecast
 const getForecast = async () => {
